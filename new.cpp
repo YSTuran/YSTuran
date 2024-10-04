9b1787e911c47366755ed6c3706463fcb3dcbc24
Yusuf S. Turan <148588002+YSTuran@users.noreply.github.com>
2024-10-04 17:11:44 +0300

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+
+
+ int main(){
+  int x,y,z;
+  cout<<"Enter the value of x: ";
+  cin>>x;
+
+  cout<<"Enter the value of y: ";
+  cin>>y;
+
+  cout<<"Enter the value of z: ";
+  cin>>z;
+  
+  cout<<"The sum of the number:"<<x+y+z<<endl;
+
+  return 0; 
+}
